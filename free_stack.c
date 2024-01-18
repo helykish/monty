e9710be64e82a3b0816f49ecbb4e3a91be3e3993
helykish <111018650+helykish@users.noreply.github.com>
2024-01-18 22:13:24 +0300

    Update free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
-* free_stack - frees a doubly linked list
-* @head: head of the stack
+* free_stack - frees a doubllinked list
+* @head: header of the stack
 */
 void free_stack(stack_t *head)
 {
