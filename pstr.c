ab6b246d179094cdadfd6367e7694020604d0b9d
helykish <111018650+helykish@users.noreply.github.com>
2024-01-18 22:18:13 +0300

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -2,9 +2,9 @@
 /**
  * f_pstr - prints the string starting at the top of the stack,
  * followed by a new
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @head: stacknode head
+ * @counter: line_no count
+ * Return: void return
 */
 void f_pstr(stack_t **head, unsigned int counter)
 {
