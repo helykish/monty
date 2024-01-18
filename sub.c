b264b62143f4ad8cd37d35848e88ddbf15a5573c
helykish <111018650+helykish@users.noreply.github.com>
2024-01-18 22:05:42 +0300

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
   *f_sub- sustration
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
+  *@head: stack nodehead
+  *@counter: line_no
+  *Return: void return
  */
 void f_sub(stack_t **head, unsigned int counter)
 {
