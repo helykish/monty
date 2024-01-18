9423d3cf379b58054aa56a4d08ed26edf349a290
helykish <111018650+helykish@users.noreply.github.com>
2024-01-18 22:09:09 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -2,8 +2,8 @@
 /**
  * f_queue - prints the top
  * @head: stack head
- * @counter: line_number
- * Return: no return
+ * @counter: line_no
+ * Return: void return
 */
 void f_queue(stack_t **head, unsigned int counter)
 {
@@ -13,10 +13,10 @@ void f_queue(stack_t **head, unsigned int counter)
 }
 
 /**
- * addqueue - add node to the tail stack
+ * addqueue - more node to tail stack
  * @n: new_value
- * @head: head of the stack
- * Return: no return
+ * @head: header of the stack
+ * Return: void return
 */
 void addqueue(stack_t **head, int n)
 {
